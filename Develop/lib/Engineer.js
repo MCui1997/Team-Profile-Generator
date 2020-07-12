bc9ac017019a7c366f3dd5a6c18627a186758c53
Michael Cui <cuimichael500@gmail.com>
2020-07-11 20:26:41 -0500

    added engineer

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,23 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+//Want to inherit from employee class
+class Engineer extends Employee{
+    constructor(name, id, email, gitHub){
+
+        //Super will call the functions from employee
+        super (name,id,email)
+        this.gitHub = gitHub;
+    }
+    getGitHub(){
+
+        return this.gitHub;
+    }
+    getRole(){
+
+        return "Engineer";
+    }
+
+}
+
+module.exports = Engineer;
